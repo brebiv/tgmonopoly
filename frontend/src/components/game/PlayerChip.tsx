fbc18f1687f5575c3ebbeef6f1a816a95506c089
Leonid <brebiv@gmail.com>
2024-10-03 23:11:56 +0200

    Change player movement speed

--- frontend/src/components/game/PlayerChip.tsx
+++ frontend/src/components/game/PlayerChip.tsx
@@ -10,6 +10,7 @@ function PlayerChip({
   top: number | undefined;
 }) {
   const [primaryColor, borderColor] = PLAYER_CHIP_COLORS[color as keyof typeof PLAYER_CHIP_COLORS];
+
   return (
     <div
       className="absolute z-10 h-3 w-3 rounded-full outline outline-2"
@@ -18,7 +19,8 @@ function PlayerChip({
         outlineColor: borderColor,
         left: left,
         top: top,
-        transition: `left ${PLAYER_CHIP_MOVE_DURATION_MS}ms, top ${PLAYER_CHIP_MOVE_DURATION_MS}ms`,
+        transition: `left ease-in-out, top ease-in-out`,
+        transitionDuration: `${PLAYER_CHIP_MOVE_DURATION_MS}ms`,
         // boxShadow: `0px 0px 8px 2px rgba(0,0,0,0.55)`,
       }}
     ></div>

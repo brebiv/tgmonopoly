9bb6d1a9b36f24bb137abbcba08236324c4fb67c
Leonid <brebiv@gmail.com>
2024-10-22 14:44:02 +0200

    Added showdow to BoardCenter

--- frontend/src/components/game/BoardCenter.tsx
+++ frontend/src/components/game/BoardCenter.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+import { hexToRGBA } from "@/lib/utils";
 import { useLayoutStore } from "@/stores/LayoutStore";
 
 function BoardCenter({ children }: { children: React.ReactNode }) {
@@ -11,7 +13,27 @@ function BoardCenter({ children }: { children: React.ReactNode }) {
         paddingBottom: `${boardCenterPaddingY + 4}px`,
       }}
     >
-      <div className="relative h-full w-full">{children}</div>
+      <div className="relative h-full w-full">
+        <div
+          className="absolute z-10 h-2 w-full"
+          style={{
+            backgroundImage: `linear-gradient(
+          180deg,
+          ${window.Telegram.WebApp.themeParams.secondary_bg_color || "rgb(18, 17, 19)"} -10%,
+          ${hexToRGBA(window.Telegram.WebApp.themeParams.secondary_bg_color, 0) || "rgb(18, 17, 19, 0)"} 110%)`,
+          }}
+        ></div>
+        {children}
+        <div
+          className="absolute bottom-0 z-10 h-2 w-full"
+          style={{
+            backgroundImage: `linear-gradient(
+          0deg,
+          ${window.Telegram.WebApp.themeParams.secondary_bg_color || "rgb(18, 17, 19)"} -10%,
+          ${hexToRGBA(window.Telegram.WebApp.themeParams.secondary_bg_color, 0) || "rgb(18, 17, 19, 0)"} 110%)`,
+          }}
+        ></div>
+      </div>
     </div>
   );
 }

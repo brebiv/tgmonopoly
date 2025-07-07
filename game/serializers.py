ba3ffcdf84635fae6a4085b579f47b7259a7b01e
Leonid <brebiv@gmail.com>
2025-07-07 18:04:15 +0200

    Added player id in its serializer

--- game/serializers.py
+++ game/serializers.py
@@ -40,6 +40,7 @@ class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
         fields = (
+            "id",
             "position",
             "cash",
             "color",

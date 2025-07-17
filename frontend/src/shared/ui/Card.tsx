e68ab0957a96c5b9e99ea58103ff3f133e47df68
Leonid <brebiv@gmail.com>
2025-07-17 18:55:10 +0200

    Added current player indicator

--- frontend/src/shared/ui/Card.tsx
+++ frontend/src/shared/ui/Card.tsx
@@ -10,7 +10,7 @@ export const Card: React.FC<CardProps> = ({ className, children }) => {
   return (
     <div
       className={cn(
-        "border-hint bg-background [&_div]:bg-background relative flex flex-col gap-4 rounded-lg border-[1px] px-4 py-2 text-2xl",
+        "border-hint bg-background relative flex flex-col gap-4 rounded-lg border-[1px] px-4 py-2 text-2xl",
         className,
       )}
     >
--- frontend/src/shared/ui/PlayerCard.tsx
+++ frontend/src/shared/ui/PlayerCard.tsx
@@ -2,6 +2,8 @@ import type { Player } from "@/entities/types";
 import type React from "react";
 import { Avatar } from "./Avatar";
 import { Card } from "./Card";
+import { DicesIcon } from "lucide-react";
+import { useGameStore } from "@/entities/gameStore";
 
 interface PlayerCardProps {
   player?: Player;
@@ -9,9 +11,19 @@ interface PlayerCardProps {
 
 export const PlayerCard: React.FC<PlayerCardProps> = ({ player }) => {
   const empty = player == null;
+  const { game } = useGameStore();
+
+  if (!game) {
+    throw new Error("Game should be initialized");
+  }
 
   return (
     <Card className="bg-background border-none px-2">
+      {!empty && game.current_player === player.id && (
+        <div className="absolute top-0 right-0">
+          <DicesIcon className="text-primary" />
+        </div>
+      )}
       <div className="flex h-14 w-full gap-2">
         <div className="aspect-square h-full">
           <Avatar player={player} />
